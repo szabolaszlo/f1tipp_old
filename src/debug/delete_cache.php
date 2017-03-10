2e6a524a4d6eec43c668e8357646fe7584351e39
szabolaszlo <szabolaszlo@shoprenter.hu>
2017-03-10 20:02:31 +0100

    delete cache from link

---
+++ src/debug/delete_cache.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Carlos
+ * Date: 2017. 03. 10.
+ * Time: 19:32
+ */
+
+$dir = __DIR__ . '/../../cache/app';
+
+rrmdir($dir);
+
+$dir = __DIR__ . '/../../cache/twig';
+
+rrmdir($dir);
+
+/**
+ * @param $dir
+ */
+function rrmdir($dir)
+{
+    if (is_dir($dir)) {
+        $objects = scandir($dir);
+        foreach ($objects as $object) {
+            if ($object != "." && $object != "..") {
+                if (is_dir($dir . "/" . $object)) {
+                    rrmdir($dir . "/" . $object);
+                } else {
+                    unlink($dir . "/" . $object);
+                }
+            }
+        }
+        rmdir($dir);
+    }
+}
